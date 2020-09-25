0d5343a5009e6d3d77a96ba8cc7229e2f04798c7
Dmitriy1892 <kodim1892@gmail.com>
2020-09-25 21:36:32 +0300

    Repaired - notifications work on all platforms

--- app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -1,9 +1,11 @@
 package com.hfad.joke;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -11,7 +13,8 @@ import androidx.core.app.NotificationCompat;
 public class DelayedMessageService extends IntentService {
 
     public static final String EXTRA_MESSAGE = "message";
-    public static final int NOTIFICATION_ID = 5453;
+    public static final int NOTIFICATION_ID = 1;
+    public static final String CHANNEL_ID = "CHANNEL_ID";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -31,24 +34,32 @@ public class DelayedMessageService extends IntentService {
     }
 
     private void showText(final String text) {
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                        .setContentTitle(getString(R.string.question))
-                        .setContentText(text)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setVibrate(new long[] {0, 1000})
-                        .setAutoCancel(true);
+        NotificationManager notificationManager =
+                (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
 
         Intent actionIntent = new Intent(this, MainActivity.class);
         PendingIntent actionPendingIntent = PendingIntent.getActivity(this,
                 0,
                 actionIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(actionPendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle(getString(R.string.question))
+                        .setContentText(text)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setVibrate(new long[] {0, 1000})
+                        .setAutoCancel(true)
+                        .setContentIntent(actionPendingIntent);
+        createChannelIfNeeded(notificationManager);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    public static void createChannelIfNeeded(NotificationManager manager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(notificationChannel);
+        }
     }
 }
